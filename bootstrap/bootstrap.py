5328e600a1dba0e31f7cfef8027a38739591d353
GeorgianaElena <georgiana.dolocan@gmail.com>
2020-02-04 17:14:57 +0200

    test_fix

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -150,7 +150,7 @@ def main():
         pip_flags.append('--editable')
     tljh_repo_path = os.environ.get(
         'TLJH_BOOTSTRAP_PIP_SPEC',
-        'git+https://github.com/jupyterhub/the-littlest-jupyterhub.git'
+        'git+https://github.com/GeorgianaElena/the-littlest-jupyterhub.git@check_req_fix'
     )
 
     run_subprocess([
